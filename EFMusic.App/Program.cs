3221ee75b75e95df88aec24961d62161bd146fd4
Angelo Gemelli <angelo.gemelli@bitspa.it>
2025-10-16 11:30:46 +0200

    add insert

--- EFMusic.App/Program.cs
+++ EFMusic.App/Program.cs
@@ -46,7 +46,7 @@ namespace EFMusic.App
                                 .Include(s => s.Album)
                                 .Include(s => s.Genres);
 
-                            Console.WriteLine(JsonSerializer.Serialize(songs,_options));
+                            Console.WriteLine(JsonSerializer.Serialize(songs, _options));
                             break;
                         case 2:
                             Console.WriteLine("Title, Artist or Genre: ");
@@ -57,23 +57,81 @@ namespace EFMusic.App
                                 .Include(s => s.Album)
                                 .Include(s => s.Genres);
 
-                            Console.WriteLine(JsonSerializer.Serialize(searchSongs,_options));
+                            Console.WriteLine(JsonSerializer.Serialize(searchSongs, _options));
                             break;
                         case 3:
                             Console.Write("Title: ");
                             var inputTitle = Console.ReadLine();
-                            Console.Write("Artist: ");
-                            var inputArtist = Console.ReadLine();
-                            Console.Write("Genres: ");
-                            var inputGenres = Console.ReadLine();
+
+                            Console.WriteLine("How many Artists: ");
+                            var inputArtistsCount = Convert.ToInt16(Console.ReadLine());
+
+                            Console.WriteLine("Avilable Artists: ");
+                            foreach (var artist in artists)
+                            {
+                                Console.WriteLine($"[{artist.Id}] {artist.Name}");
+                            }
+                            ICollection<int> inputArtistsIds = [];
+
+                            for (int i = 0; i < inputArtistsCount; i++)
+                            {
+                                Console.Write("Artist Id: ");
+                                inputArtistsIds.Add(Convert.ToInt32(Console.ReadLine()));
+                            }
+                            var inputArtists = context.Artists.Where(a => inputArtistsIds.Contains(a.Id)).ToList();
+
+                            Console.WriteLine("How many Genres: ");
+                            var inputGenresCount = Convert.ToInt16(Console.ReadLine());
+                            foreach (var genre in genres)
+                            {
+                                Console.WriteLine($"[{genre.Id}] {genre.Name}");
+                            }
+                            ICollection<int> inputGenresIds = [];
+
+                            for (int i = 0; i < inputGenresCount; i++)
+                            {
+                                Console.Write("Genres Id: ");
+                                inputGenresIds.Add(Convert.ToInt32(Console.ReadLine()));
+                            }
+                            var inputGenres = context.Genres.Where(g => inputGenresIds.Contains(g.Id)).ToList();
+
                             Console.Write("Length: ");
-                            var inputLength = Console.ReadLine();
-                            Console.Write("Producer: ");
-                            var inputProducer = Console.ReadLine();
+                            var inputLength = Convert.ToInt16(Console.ReadLine());
+
+                            Console.Write("Available Producers: ");
+                            foreach (var producer in producers)
+                            {
+                                Console.WriteLine($"[{producer.Id}] {producer.Name}");
+                            }
+                            Console.Write("Producer Id:");
+                            var inputProducerId = Convert.ToInt16(Console.ReadLine());
+                            var inputProducer = context.Producers.Where(p => p.Id == inputProducerId).First();
+
                             Console.Write("Release Date: ");
-                            var inputReleaseDate = Console.ReadLine();
-                            Console.Write("Album: ");
-                            var inputAlbum = Console.ReadLine();
+                            var inputReleaseDate = Convert.ToDateTime(Console.ReadLine());
+
+                            Console.WriteLine("Available Albums: ");
+                            foreach (var album in albums)
+                            {
+                                Console.WriteLine($"[{album.Id}] {album.Name}");
+                            }
+
+                            Console.Write("Album Id: ");
+                            var inputAlbumId = Convert.ToInt32(Console.ReadLine());
+                            var inputAlbum = context.Albums.Where(a => a.Id == inputAlbumId).First();
+
+                            var song = new Song()
+                            {
+                                Title = inputTitle,
+                                SongDetails = new SongDetails() { Length = inputLength, ReleaseDate = inputReleaseDate, Producer = inputProducer },
+                                Artists = inputArtists,
+                                Album = inputAlbum,
+                                Genres = inputGenres
+                            };
+
+                            context.Songs.Add(song);
+                            context.SaveChanges();
+
                             break;
                         case 4:
                             return;
